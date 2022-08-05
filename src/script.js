8c2f1aa2ccaa1f67a57adc21e0459b0984a47133
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-08-06 00:31:31 +0300

    api change

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 let now = new Date();
 const cityPlace = document.querySelector("#search_city");
 const apiDomain = "https://api.openweathermap.org";
-const api = "05872d6d96180a5ad27603c03bb9c9a8";
+const api = "baa17103f06129d63a6c32b2406b94ba";
 const days = [
   "Sunday",
   "Monday",
@@ -120,7 +120,7 @@ function getPosition(response) {
   console.log(response);
   let lat = response.coords.latitude;
   let long = response.coords.longitude;
-  let urlPosition = `${apiDomain}/data/2.5/weather?lat=${lat}&lon=${long}exclude=daily&appid=05872d6d96180a5ad27603c03bb9c9a8&units=metric`;
+  let urlPosition = `${apiDomain}/data/2.5/weather?lat=${lat}&lon=${long}exclude=daily&appid=baa17103f06129d63a6c32b2406b94ba&units=metric`;
   axios.get(urlPosition).then(showWeather);
 }
 
